c2794c47897e6d3e510f102a06e85d92e376c544
Natalikud2 <Reimova091@mail.ru>
2023-07-14 12:28:47 +0800

    Version 2

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//import './css/style.css';
+//*ошибка 
+//Загрузка модуля по адресу «http://127.0.0.1:8080/css/style.css» 
+//была заблокирована из-за неразрешенного MIME-типа («text/css»).
+//решение: подключить стиль в html по тегу link
+
+import './App/app.js';
+console.log('index log');
+
+//mozilla firefox не работает
+//яндекс браузер - game started
